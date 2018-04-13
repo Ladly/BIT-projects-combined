bb9153c7bc40fd4f0c2bca098065cf4bb3090a90
Ladly <vukivanovic.vuk@gmail.com>
2018-04-13 09:04:11 +0200

    T7 Create footer

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, Link } from "react-router-dom";
 import { Navigation } from './components/Navigation'
 import Homepage from './containers/Homepage'
 import ShowDetailsPage from './containers/ShowDetailsPage'
+import { Footer } from './components/Footer'
 
 
 export const App = () => {
@@ -15,6 +16,7 @@ export const App = () => {
                 <Route exact path="/" component={Homepage}/>
                 <Route exact path="/details/:id" component={ShowDetailsPage}/>
             </Switch>
+            <Footer />
         </Fragment>
     )
 }
